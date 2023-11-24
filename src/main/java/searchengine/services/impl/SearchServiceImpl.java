16f6529829fbedf27d4ed10884bcdda6e75a6cef
Viacheslav Sles <slesv@yandex.ru>
2023-11-24 20:18:05 +0700

    Search for all site is done without snippets.

--- src/main/java/searchengine/services/impl/SearchServiceImpl.java
+++ src/main/java/searchengine/services/impl/SearchServiceImpl.java
@@ -28,73 +28,79 @@ public class SearchServiceImpl implements SearchService {
     private final IndexRepository indexRepository;
     private final SitesList sites;
 
-//    @Transactional
     @Override
     public SearchResponse search(String query, String site, Integer offset, Integer limit) {
         Set<String> searchingLemmas =  new LemmaSearcherImpl().getLemmas(query);
-        List<Map<Lemma, Float>> findingLemmasOnSites = new ArrayList<>();
-//        ToDo: Сделать выборку для нескольких сайтов
-        Map<Lemma, Float> findingLemmas = new TreeMap<>(Comparator.comparing(Lemma::getFrequency));
+        List<Map<Page, Float>> findingPagesWithRelevanceForSites = new ArrayList<>();
         for (SiteCfg siteCfg : sites.getSites()) {
-            findingLemmas = findLemmasOnSite(site, siteCfg, searchingLemmas);
-            if (searchingLemmas.size() == findingLemmas.size()) {
-                findingLemmasOnSites.add(findingLemmas);
-            } else {
+            if (!site.isEmpty() && !site.equals(siteCfg.getUrl())) {
                 continue;
             }
-
+            Map<Lemma, Float> findingLemmas = findLemmasOnSite(siteCfg, searchingLemmas);
+            if (searchingLemmas.size() != findingLemmas.size()) {
+                continue;
+            }
+            findingLemmas.forEach((key, value) -> System.out.println(key + " - " + value));
+//            Исключение часто встречающихся лемм. В поисковой выдаче попадаются результаты без этих лемм.
+//            findingLemmas.entrySet().removeIf(entry -> entry.getValue() > 0.1f && findingLemmas.size() > 2);
+            Map<Page, Float> pagesWithRelevance = findPagesWithRelevance(findingLemmas);
+            System.out.println("Количество найденных страниц: " + pagesWithRelevance.size());
+//        pagesWithRelevance.forEach((k, v) -> System.out.println(k.getId() + " - " + k.getPath() + " - " + k.getSite().getUrl() + " - " + v));
+            if (!pagesWithRelevance.isEmpty()) {
+                findingPagesWithRelevanceForSites.add(pagesWithRelevance);
+            }
         }
-//        if (searchingLemmas.size() != findingLemmas.size()) {
-//            return createEmptyListResponse();
-//        }
-        findingLemmas.forEach((key, value) -> System.out.println(key + " - " + value));
-        findingLemmas.entrySet().removeIf(entry -> entry.getValue() > 0.1 && findingLemmas.size() > 2);
+        if (findingPagesWithRelevanceForSites.isEmpty()) {
+            return createEmptyListResponse();
+        }
+        Map<Page, Float> pagesWithRelevanceForAllSites = joinAllResultPages(findingPagesWithRelevanceForSites);
+        calculateRelativelyRelevance(pagesWithRelevanceForAllSites);
+        List<Map.Entry<Page, Float>> reducedPagesWithRelevance =
+                reduceAllPages(pagesWithRelevanceForAllSites, offset, limit);
+        List<DetailedSearchItem> searchItems = createSearchItems(reducedPagesWithRelevance);
+        return SearchResponse.builder()
+                .count(pagesWithRelevanceForAllSites.size())
+                .data(searchItems)
+                .build();
+    }
+
+    @Transactional
+    public Map<Lemma, Float> findLemmasOnSite(SiteCfg siteCfg, Set<String> searchingLemmas) {
+        Map<Lemma, Float> findingLemmas = new TreeMap<>(Comparator.comparing(Lemma::getFrequency));
+        Optional<Site> searchingSiteOpt = siteRepository.findSiteByUrl(siteCfg.getUrl());
+        if (searchingSiteOpt.isEmpty() || searchingSiteOpt.get().getStatus() != Status.INDEXED) {
+//                    ToDo: Сгенерировать ошибки по запросу
+            return findingLemmas;
+        }
+        int countPages = pageRepository.countPages(searchingSiteOpt.get().getId());
+        for (String lemmaStr : searchingLemmas) {
+            Optional<Lemma> findingLemmaOpt = lemmaRepository
+                    .findLemmaBySiteIdAndLemma(searchingSiteOpt.get().getId(), lemmaStr);
+            findingLemmaOpt
+                    .ifPresent(lemma -> findingLemmas.put(lemma, (float) lemma.getFrequency() / countPages));
+        }
+        return findingLemmas;
+    }
+
+    private Map<Page, Float> findPagesWithRelevance(Map<Lemma, Float> findingLemmas) {
         Map<Page, Float> pagesWithRelevance = new HashMap<>();
         boolean firstLemma = true;
         for (Lemma lemma : findingLemmas.keySet()) {
             if (firstLemma) {
                 firstLemma = false;
                 findPagesWithFirstLemma(pagesWithRelevance, lemma);
-                continue;
+            } else {
+                pagesWithRelevance = findLemmaOnPages(pagesWithRelevance, lemma);
             }
             if (pagesWithRelevance.isEmpty()) {
-                break;
+                return pagesWithRelevance;
             }
-            pagesWithRelevance = findLemmaOnPages(pagesWithRelevance, lemma);
         }
-        System.out.println("Количество найденных страниц: " + pagesWithRelevance.size());
-//        pagesWithRelevance.forEach((k, v) -> System.out.println(k.getId() + " - " + k.getPath() + " - " + k.getSite().getUrl() + " - " + v));
-        if (pagesWithRelevance.isEmpty()) {
-            return createEmptyListResponse();
-        }
-        Float maxAbsRelevance = pagesWithRelevance.values().stream().max(Float::compareTo).get();
-//        ToDo: На этом этапе можно сливать мапы с разных сайтов в одну
-        pagesWithRelevance.entrySet().forEach(entry -> entry.setValue(entry.getValue() / maxAbsRelevance));
-        List<Map.Entry<Page, Float>> sortedPagesWithRelevance = new ArrayList<>(pagesWithRelevance.entrySet().stream().toList());
-        sortedPagesWithRelevance.sort((e1, e2) -> -e1.getValue().compareTo(e2.getValue()));
-        List<Map.Entry<Page, Float>> reducedPagesWithRelevance = sortedPagesWithRelevance.stream()
-                .skip(offset).limit(limit).toList();
-        reducedPagesWithRelevance.forEach((entry) -> System.out.println(entry.getKey().getId() + " - " + entry.getKey().getPath() + " - " + entry.getKey().getSite().getUrl() + " - " + entry.getValue()));
-        List<DetailedSearchItem> searchItems = new ArrayList<>();
-        for (Map.Entry<Page, Float> pageWithRelevance : reducedPagesWithRelevance) {
-            DetailedSearchItem searchItem = DetailedSearchItem.builder()
-                    .site(pageWithRelevance.getKey().getSite().getUrl())
-                    .siteName(pageWithRelevance.getKey().getSite().getName())
-                    .uri(pageWithRelevance.getKey().getPath())
-                    .title(Jsoup.parse(pageWithRelevance.getKey().getContent()).title())
-                    .snippet("getSnippet(pageWithRelevance.getKey().getContent(), searchingLemmas)")
-                    .relevance(pageWithRelevance.getValue())
-                    .build();
-            searchItems.add(searchItem);
-        }
-        searchItems.sort(Comparator.comparing(DetailedSearchItem::getRelevance).reversed());
-        return SearchResponse.builder()
-                .count(pagesWithRelevance.size())
-                .data(searchItems)
-                .build();
+        return pagesWithRelevance;
     }
 
-    private void findPagesWithFirstLemma(Map<Page, Float> pagesWithRelevance, Lemma lemma) {
+    @Transactional
+    public void findPagesWithFirstLemma(Map<Page, Float> pagesWithRelevance, Lemma lemma) {
         List<Index> indices = indexRepository.findIndexByLemmaId(lemma.getId());
         if (!indices.isEmpty()) {
             for (Index index : indices) {
@@ -103,7 +109,8 @@ public class SearchServiceImpl implements SearchService {
         }
     }
 
-    private Map<Page, Float> findLemmaOnPages(Map<Page, Float> pagesWithRelevance, Lemma lemma) {
+    @Transactional
+    public Map<Page, Float> findLemmaOnPages(Map<Page, Float> pagesWithRelevance, Lemma lemma) {
         Map<Page, Float> reducedPagesWithRelevance = new HashMap<>();
         for (Page page : pagesWithRelevance.keySet()) {
             Optional<Index> indexOpt = indexRepository
@@ -121,28 +128,51 @@ public class SearchServiceImpl implements SearchService {
                 .build();
     }
 
+    private Map<Page, Float> joinAllResultPages(List<Map<Page, Float>> findingPagesWithRelevanceForSites) {
+        Map<Page, Float> pagesWithRelevanceForAllSites = new HashMap<>();
+        for (Map<Page, Float> pagesWithRelevance : findingPagesWithRelevanceForSites) {
+            pagesWithRelevanceForAllSites.putAll(pagesWithRelevance);
+        }
+        return pagesWithRelevanceForAllSites;
+    }
+
+    private void calculateRelativelyRelevance(Map<Page, Float> pagesWithRelevanceForAllSites) {
+        Float maxAbsRelevance = pagesWithRelevanceForAllSites.values()
+                .stream().max(Float::compareTo).get();
+        pagesWithRelevanceForAllSites.entrySet()
+                .forEach(entry -> entry.setValue(entry.getValue() / maxAbsRelevance));
+    }
+
+    private List<Map.Entry<Page, Float>> reduceAllPages(Map<Page, Float> pagesWithRelevanceForAllSites,
+                                                        Integer offset, Integer limit) {
+        List<Map.Entry<Page, Float>> sortedPagesWithRelevance = new ArrayList<>(pagesWithRelevanceForAllSites
+                .entrySet().stream().toList());
+        sortedPagesWithRelevance.sort((e1, e2) -> -e1.getValue().compareTo(e2.getValue()));
+        List<Map.Entry<Page, Float>> reducedPagesWithRelevance = sortedPagesWithRelevance.stream()
+                .skip(offset).limit(limit).toList();
+        reducedPagesWithRelevance.forEach((entry) -> System.out.println(entry.getKey().getId() + " - "
+                + entry.getKey().getPath() + " - " + entry.getKey().getSite().getUrl() + " - " + entry.getValue()));
+    return reducedPagesWithRelevance;
+    }
+
+    private List<DetailedSearchItem> createSearchItems(List<Map.Entry<Page, Float>> reducedPagesWithRelevance) {
+        List<DetailedSearchItem> searchItems = new ArrayList<>();
+        for (Map.Entry<Page, Float> pageWithRelevance : reducedPagesWithRelevance) {
+            DetailedSearchItem searchItem = DetailedSearchItem.builder()
+                    .site(pageWithRelevance.getKey().getSite().getUrl())
+                    .siteName(pageWithRelevance.getKey().getSite().getName())
+                    .uri(pageWithRelevance.getKey().getPath())
+                    .title(Jsoup.parse(pageWithRelevance.getKey().getContent()).title())
+                    .snippet("getSnippet(pageWithRelevance.getKey().getContent(), searchingLemmas)")
+                    .relevance(pageWithRelevance.getValue())
+                    .build();
+            searchItems.add(searchItem);
+        }
+        return searchItems;
+    }
+
     private String getSnippet(String content, Set<String> searchingLemmas) {
         LemmaSearcher lemmaSearcher = new LemmaSearcherImpl();
         return lemmaSearcher.getSnippet(content, searchingLemmas);
-    }
-
-    private Map<Lemma, Float> findLemmasOnSite(String site, SiteCfg siteCfg, Set<String> searchingLemmas) {
-        Map<Lemma, Float> findingLemmas = new TreeMap<>(Comparator.comparing(Lemma::getFrequency));
-        if (!site.isEmpty() && !site.equals(siteCfg.getUrl())) {
-            return findingLemmas;
-        }
-        Optional<Site> searchingSiteOpt = siteRepository.findSiteByUrl(siteCfg.getUrl());
-        if (searchingSiteOpt.isEmpty() || searchingSiteOpt.get().getStatus() != Status.INDEXED) {
-//                    ToDo: Сгенерировать ошибки по запросу
-            return findingLemmas;
-        }
-        int countPages = pageRepository.countPages(searchingSiteOpt.get().getId());
-        for (String lemmaStr : searchingLemmas) {
-            Optional<Lemma> findingLemmaOpt = lemmaRepository
-                    .findLemmaBySiteIdAndLemma(searchingSiteOpt.get().getId(), lemmaStr);
-            findingLemmaOpt
-                    .ifPresent(lemma -> findingLemmas.put(lemma, (float) lemma.getFrequency() / countPages));
-        }
-        return findingLemmas;
     }
 }
